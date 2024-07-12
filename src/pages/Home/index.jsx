f91c4fc4b1d5d1c60d47f20e99054b86f9393ce0
roschel <joaomroschel@gmail.com>
2024-07-12 19:06:51 -0300

    criando button com styled-components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,12 @@
-import {Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { Button } from "../../components/Button";
+
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <Button title={"Teste 1"}/>
+            <Button variant="secondary" title={"Teste 2"} />
             <Link to="/login" >Fazer login</Link>
         </>
     )
