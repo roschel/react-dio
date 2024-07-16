bb8fe04d8832cf02ef9796187fb79831a40b5d38
roschel <joaomroschel@gmail.com>
2024-07-16 19:47:16 -0300

    completando home

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  max_width: 80%;
+  max-width: 80%;
   height: 47px;
 
   display: flex;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,31 @@
-import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Container, TextContent, Title, TitleHighlight } from "./styles";
+import banner from "../../assets/banner.png";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Button title={"Teste 1"} />
-      <Button variant="secondary" title={"Teste 2"} />
-      <Link to="/login">Fazer login</Link>
+      <Container>
+        <div>
+          <Title>
+            <TitleHighlight>
+              Implemente <br />
+            </TitleHighlight>
+            o seu futuro global agora!
+          </Title>
+          <TextContent>
+            Domine as tecnologias utilizadas pelas empresas mais inovadoras do
+            mundo e encare seu novo desafio profissional, evoluindo em
+            comunidade com os melhores experts
+          </TextContent>
+          <Button title="Começar agora" variant="secondary" />
+        </div>
+        <div>
+          <img src={banner} alt="Imagem Principal" />
+        </div>
+      </Container>
     </>
   );
 };
