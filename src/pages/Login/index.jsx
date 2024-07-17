9ae493d9cfbcbd6fd60281e98e4cbfd2379a873c
roschel <joaomroschel@gmail.com>
2024-07-17 19:35:05 -0300

    inserindo navegacao

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { MdEmail, MdLock } from "react-icons/md";
 
 import { Button } from "../../components/Button";
@@ -16,6 +17,12 @@ import {
 } from "./styles";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleClickSignIn = () => {
+    navigate("/feed");
+  };
+
   return (
     <>
       <Header />
@@ -37,7 +44,12 @@ const Login = () => {
                 type="password"
                 leftIcon={<MdLock />}
               />
-              <Button title={"Entrar"} variant="secondary" />
+              <Button
+                title={"Entrar"}
+                variant="secondary"
+                onClick={handleClickSignIn}
+                type="button"
+              />
             </form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
