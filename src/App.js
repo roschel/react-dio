0255c469807dc92471118cb8df99301f25c9bfc2
roschel <joaomroschel@gmail.com>
2024-07-17 18:47:32 -0300

    Adicionando Feed nas rotas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { Feed } from "./pages/Feed";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
         </Routes>
       </div>
     </Router>
