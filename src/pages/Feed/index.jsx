8bce5af291d4e616e5e3590943702aae13221b6e
roschel <joaomroschel@gmail.com>
2024-07-17 18:53:28 -0300

    Adicionando página de Feed

---
+++ src/pages/Feed/index.jsx
@@ -0,0 +1,17 @@
+import { Card } from "../../components/Card";
+import { Header } from "../../components/Header";
+import { Container, TextContent, Title, TitleHighlight } from "./styles";
+import banner from "../../assets/banner.png";
+
+const Feed = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Card />
+      </Container>
+    </>
+  );
+};
+
+export { Feed };
