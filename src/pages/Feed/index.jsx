97d67accd622cfb660fb91372c91f3ee1cae1836
roschel <joaomroschel@gmail.com>
2024-07-17 19:34:49 -0300

    inserindo logica de autenticacao

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,14 +1,62 @@
 import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
-import { Container, TextContent, Title, TitleHighlight } from "./styles";
-import banner from "../../assets/banner.png";
+import { UserInfo } from "../../components/UserInfo";
+import { Container, Column, Title, TitleHighlight } from "./styles";
 
 const Feed = () => {
   return (
     <>
-      <Header />
+      <Header autenticado={true} />
       <Container>
-        <Card />
+        <Column flex={3}>
+          <Title>Feed</Title>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Column>
+        <Column flex={1}>
+          <TitleHighlight># RANKING 5 TOP DA SEMANA</TitleHighlight>
+          <UserInfo
+            percentual={90}
+            nome={"João"}
+            image={
+              "https://avatars.githubusercontent.com/u/52433168?s=400&u=1224e8cabc7a1780c42610345c8ce4b667f07b7b&v=4"
+            }
+          />
+          <UserInfo
+            percentual={80}
+            nome={"João"}
+            image={
+              "https://avatars.githubusercontent.com/u/52433168?s=400&u=1224e8cabc7a1780c42610345c8ce4b667f07b7b&v=4"
+            }
+          />
+          <UserInfo
+            percentual={70}
+            nome={"João"}
+            image={
+              "https://avatars.githubusercontent.com/u/52433168?s=400&u=1224e8cabc7a1780c42610345c8ce4b667f07b7b&v=4"
+            }
+          />
+          <UserInfo
+            percentual={60}
+            nome={"João"}
+            image={
+              "https://avatars.githubusercontent.com/u/52433168?s=400&u=1224e8cabc7a1780c42610345c8ce4b667f07b7b&v=4"
+            }
+          />
+          <UserInfo
+            percentual={50}
+            nome={"João"}
+            image={
+              "https://avatars.githubusercontent.com/u/52433168?s=400&u=1224e8cabc7a1780c42610345c8ce4b667f07b7b&v=4"
+            }
+          />
+        </Column>
       </Container>
     </>
   );
--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 export const Container = styled.main`
   width: 100%;
@@ -9,33 +9,29 @@ export const Container = styled.main`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
 `;
 
-export const Title = styled.h2`
+export const Title = styled.h3`
   font-family: "Open Sans";
   font-style: normal;
   font-weight: 700;
-  font-size: 32px;
-  width: 320px;
-  margin-bottom: 20px;
-  line-height: 44px;
-
+  font-size: 18px;
+  line-height: 25px;
   color: #ffffff;
+  margin-bottom: 24px;
 `;
 
-export const TitleHighlight = styled.span`
-  color: #e4105d;
-`;
-
-export const TextContent = styled.p`
+export const TitleHighlight = styled.h3`
   font-family: "Open Sans";
   font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  width: 420px;
-  margin-bottom: 20px;
-  line-height: 22px;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 25px;
+  color: #ffffff70;
+  margin-bottom: 24px;
+`;
 
-  color: #ffffff;
+export const Column = styled.div`
+  flex: ${({ flex }) => flex};
+  padding-right: 24px;
 `;
