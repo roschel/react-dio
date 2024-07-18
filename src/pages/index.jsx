5cdc96df79c3c24c0473aaae54f828b3d53271f7
roschel <joaomroschel@gmail.com>
2024-07-18 11:29:29 -0300

    useCallBack

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
 const Test = () => {
+  const [name, setName] = useState("João");
   const [age, setAge] = useState(32);
 
-  const handleChangeName = () => {
-    setAge((prev) => (prev === 32 ? 26 : 32));
-  };
+  // useMemo = referencia em variáveis(const, let, var)
+  // useCallBack = referencia de função
 
-  const changeUser = () => {
-    handleChangeName();
-  };
-
-  const calculo = useMemo(() => {
-    console.log("calculou", age);
-    return 10 * age;
-  }, [age]);
-
-  console.log("renderizou", calculo);
-
-  useEffect(() => {
-    changeUser();
+  const handleChangeName = useCallback(() => {
+    setName((prev) => (prev === "João" ? "Pablo" : "João"));
   }, []);
+
+  const handleChangeAge = useCallback(() => {
+    const newAge = 10 * age;
+    console.log("Age atual", age, newAge);
+    setAge((prev) => (prev === 32 ? 26 : 32));
+  }, [age]);
 
   return (
     <div>
-      <p>{age}</p>
-      <button onClick={handleChangeName}>Alterar</button>
+      <p>Idade: {age}</p>
+      <br />
+      <p>Nome: {name}</p>
+      <br />
+      <button onClick={handleChangeAge}>Alterar idade</button>
+      <br />
+      <button onClick={handleChangeName}>Alterar nome</button>
     </div>
   );
 };
